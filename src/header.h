0635d384355799af34a7ffa877b08c67533e4f62
ApekTahua <kelvin.hartono@binus.ac.id>
2023-12-11 09:47:01 +0700

    Update platformio.ini and header.h files

--- src/header.h
+++ src/header.h
@@ -9,10 +9,64 @@
 #include <SoftwareSerial.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
-#include <freertos/semphr.h>
+#include <WiFi.h>
 #include <MAX7219_7Seg_Disp.h>
-// #include <FirebaseESP32.h>
-// #include <NTPClient.h>
+#include <FirebaseESP32.h>
+#include <NTPClient.h>
+
+// Provide the token generation process info.
+#include "addons/TokenHelper.h"
+// Provide the RTDB payload printing info and other helper functions.
+#include "addons/RTDBHelper.h"
+
+// NTP
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP);
+
+// WiFi
+#define WIFI_SSID "Apek"
+#define WIFI_PASSWORD "kelvin108"
+
+// Firebase
+#define DATABASE_URL "https://esp-scale-default-rtdb.asia-southeast1.firebasedatabase.app/"
+#define API_KEY "AIzaSyA3lJ_uKI-s_QASFbfv4j-SsPjGD4Vm6aM"
+
+#define FIREBASE_PROJECT_ID "esp-scale"
+#define FIREBASE_CLIENT_EMAIL "firebase-adminsdk-62ho0@esp-scale.iam.gserviceaccount.com"
+const char PRIVATE_KEY[] PROGMEM = "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDZvM5135im8iY+\n6dp5/jCF24R/QYNSngVN7U/rvqx4ejPGU9ozvIjtVuUvxC6GPkRBui+m63RD/RGx\nB+hWphwedpQzBwgiumNdTjZd48XpKCO6KLiVNlyxMzS3AomyCn0Km5UhA41x7t9U\nX/XVwWRZYV4XTBzktnwH1AOJoSo2ImuK4Avb+Dh7AvT6lojPxrt/kJbPlTkLywZl\nNQlhR/vgStO2tGSYDwbfmQSFylaUBY8Vxgnm/vlhI3ar9V05oetn9gMQhMXdPWWD\nhKXugtIK0/GEVPfiQJSxiLDXrC27VSwlWgeAKyAsWQbCLi9ekkqjWRSHRSREwsRj\niJETJnsRAgMBAAECggEAS6dJiTU2ghMy7LsuWSLT0z6xq9Gkgmk7aJcjJMMsg/PZ\n13g9IarJXMXLfixJCgXKi5fYhMPcst7UbdFUjf7LWF5VIeG4lvByI/JP+5Fvq5Jx\nz3XkpJut2zfyggAUvdLdHXlveHgWhla8LEHnVe0LI+8r1SZyB5nMQKAO1pwz4Xiy\nBuHEd1ojYVJgkRlcE4sDQAv5oYkZsIB5xL37VLTFxIshuzyIcc0Vrph/WWNzcizf\n6omoTYzJVTtWhPDn+tnMtxxY3t5ZN5lLXgzmskT1s6mN1GLe2K2QWPHu6Cwm8eY8\nLagx4OIkPk2+hbK07kPbBUEa38WgNSY5MlG+M8Fc3QKBgQDzXd3SJbiWNrnQbysY\nu7c/KbJqSyHK1F17pEeDVUN/ZKzswb/04NMPA8+HurzCRV9gc2x2IG9vhJ3yS63d\n/i4R3HeSuNxdABSe2KSgtsYqb2vMdMXl7FeE6NvCOpm24VMgLOd4Nc35dsggUChU\nFY2t4dLWzhk6dNlqFlSUW72FQwKBgQDlClnMsXx7BpQ+N4Hoye1j0K1Ok3ZlX4zQ\ns4HayMDV6ZF8zWDzij9hZO5DCtsRuFXGlNdkdEP62NZ3BzwkoflkO1XJYSqbRnjY\nNoN2/RP/O/1tOemI7rMNkx0cMF52G+ijXIi8Iqq1/P0qiRQSNFtgsq7bvHieyHm5\nLlrNHFOPGwKBgBjfXQ5fQJBJo5AD7Jmohb/02QE4EIoUTkGWtqBblHxuo6XG34F9\ngNSmBQ3QLf6E7IyE/27LPdhd626aAXkQw1CcAbSDJDJ1EaZyadDrHTG1FYVd4ePv\n1boFuoANUSx5tu8w569HgHeghi+XQafmVAaTb+L4SWfKDXZkNdy7y5DVAoGBANtI\naqtALja3GMzsZdMTKNWMn7CAHDV4IqBZjHSECaODyIfdNfpVHNNJR5gmV5EClBTY\nwdNqQP90BPjWk21bKiMdb8eq4JjjAqW2o20TZcbSj5K4hG9WYVUYySEI6hdYJwXo\nfPzqj9hHC7dioDXBrM7KpAC61BzpQSjbSZHgkduLAoGBAK1KwNa7PcSolpjmU3Ro\npmYX4tSJikMfbZqZPMtYp4v1ZbpmnBNWbm72Vx+CXw/BoW2URqtSUQo2oO9Tr1ng\nhJ5Tw0TMLeeVyPGSgTLqxuRPgeH6J/3WwFJI07E6z1/ts47LIh2Rp7moANclrQ1o\n79ggmuUjtwW+Gj8Rz7P1PX04\n-----END PRIVATE KEY-----\n";
+
+SemaphoreHandle_t mutex = xSemaphoreCreateMutex();
+SemaphoreHandle_t sSemaphore = xSemaphoreCreateBinary();
+SemaphoreHandle_t cSemaphore = xSemaphoreCreateCounting(4, 4);
+
+FirebaseJson json;
+FirebaseData firebaseData;
+FirebaseAuth auth;
+FirebaseConfig config;
+
+String bin1Path = "/Read/Tong1";
+String bin1PickCount = "/Counter/fullrec";
+String logIndexPath = "/Read/Tong1/index";
+String hourPath = "/Read/Tong1/hour";
+String minutePath = "/Read/Tong1/minute";
+String pickupStatus;
+String checkPickupStatus;
+String status;
+String date;
+String day;
+String dateFormat;
+
+int hour;
+int currentHour;
+int nextHour;
+int minute;
+int currentMinute;
+int nextMinute;
+int currentSecond;
+int daynumber;
+
+// Relay
+#define RELAY 26
 
 // UltraSonic
 #define TRIG_PIN 14
@@ -25,13 +79,17 @@
 Servo servo;
 #define SERVO_PIN 33
 
-// 7-Segment
+// 7 Segment
 MAX7219_7Seg_Disp disp(23, 5, 18);
+int wdigit1, wdigit2, wfraction1, wfraction2, pdigit1, pdigit2;
 
 // HX711
 #define LOADCELL_DOUT_PIN 15
 #define LOADCELL_SCK_PIN 22
-// float calib = 18.51;
+HX711 scale;
+float weight;
+float kg;
+float calib = 22.73;
 
 // GPS
 #define GPS_RX 16
@@ -40,28 +98,14 @@ MAX7219_7Seg_Disp disp(23, 5, 18);
 TinyGPSPlus gps;
 double latitude;
 double longitude;
+// The serial connection to the GPS device
 SoftwareSerial ss(GPS_RX, GPS_TX);
 
-// Relay
-#define RELAY 26
-
 // UltraSonic settings
-#define height 100
+#define height 60
 #define SOUND_SPEED 0.034
 #define CM_TO_INCH 0.393701
 
 int jarak;
 int percentage;
-float weight;
-float kg;
-int w_LSD, w_CSD, w_MSD, w_XSD, per_LSD, per_MSD, per_fraction;
 long duration;
-
-HX711 scale;
-
-// Semaphore
-SemaphoreHandle_t ultrasonicSemaphore;
-SemaphoreHandle_t openBinSemaphore;
-SemaphoreHandle_t printStatusSemaphore;
-
-volatile bool updateDisplayFlag;
--- src/main.cpp
+++ src/main.cpp
@@ -1,83 +1,48 @@
 #include "header.h"
 
-void ultrasonic()
+void wifiSetup()
 {
-    if (xSemaphoreTake(ultrasonicSemaphore, portMAX_DELAY))
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(WIFI_SSID);
+
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    WiFi.mode(WIFI_STA);
+
+    while (WiFi.status() != WL_CONNECTED)
     {
-        digitalWrite(TRIG_PIN, LOW);
-        delayMicroseconds(10);
-        digitalWrite(TRIG_PIN, HIGH);
-        delayMicroseconds(10);
-        digitalWrite(TRIG_PIN, LOW);
-
-        duration = pulseIn(ECHO_PIN, HIGH);
-        jarak = duration * SOUND_SPEED / 2;
-
-        percentage = (height - jarak) * 100 / height;
-
-        xSemaphoreGive(ultrasonicSemaphore);
+        delay(500);
+        Serial.print(".");
     }
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
 }
 
 void Open_Bin()
 {
-    if (xSemaphoreTake(openBinSemaphore, portMAX_DELAY))
+    if (digitalRead(IR_PIN) == HIGH)
     {
-        if (digitalRead(IR_PIN) == HIGH)
-        {
-            servo.write(180);
-            updateDisplayFlag = true;
-        }
-        else
-        {
-            servo.write(0);
-            updateDisplayFlag = true;
-        }
-
-        xSemaphoreGive(openBinSemaphore);
+        servo.write(180);
+    }
+    else
+    {
+        servo.write(0);
     }
 }
 
-void Seven_Segment()
-{
-    if (updateDisplayFlag)
-    {
-        if (!isnan(weight) && !isnan(percentage))
-        {
-            int w_MSD = int(kg) / 10;
-            int w_CSD = int(kg) % 10;
-            int w_XSD = (kg - int(kg)) * 10;
-            int w_LSD = int(kg * 100) % 10;
-
-            disp.Number(8, w_MSD);
-            disp.Numberdp(7, w_CSD);
-            disp.Number(6, w_XSD);
-            disp.Number(5, w_LSD);
-
-            int per_MSD = int(percentage) / 10;
-            int per_LSD = int(percentage) % 10;
-
-            disp.Number(3, per_MSD);
-            disp.Number(2, per_LSD);
-
-            updateDisplayFlag = true;
-        }
-        else
-        {
-            disp.Clear();
-        }
-    }
-}
-
-void GPS()
+void getLocation()
 {
     if (ss.available() > 0)
     {
+        Serial.println("GPS Serial Connection Available");
         gps.encode(ss.read());
         if (gps.location.isValid())
         {
-            latitude = gps.location.lat();
-            longitude = gps.location.lng();
+            latitude = (gps.location.lat());
+            longitude = (gps.location.lng());
         }
         else
         {
@@ -87,83 +52,392 @@ void GPS()
     }
     else
     {
-        Serial.println("GPS Not Available");
+        Serial.println("GPS Serial Connection Not Available");
     }
 }
 
-void printStatus()
+float roundToDecimalPlaces(float value, int decimalPlaces)
 {
-    if (xSemaphoreTake(printStatusSemaphore, portMAX_DELAY))
+    float multiplier = std::pow(10.0, decimalPlaces);
+    return std::round(value * multiplier) / multiplier;
+}
+
+void serialPrint()
+{
+    // Get Percentage
+    digitalWrite(TRIG_PIN, LOW);
+    delayMicroseconds(2);
+    digitalWrite(TRIG_PIN, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(TRIG_PIN, LOW);
+
+    duration = pulseIn(ECHO_PIN, HIGH);
+    jarak = duration * SOUND_SPEED / 2;
+
+    percentage = (height - jarak) * 100 / height;
+
+    // Get Weight
+    if (!scale.is_ready())
+    {
+        Serial.println("HX711 not found.");
+    }
+    else
     {
         weight = scale.get_units();
-        kg = weight / 1000;
+        if (weight < 0.0)
+        {
+            weight = 0.0; // Set negative readings to 0
+        }
+        kg = roundToDecimalPlaces((weight / 1000), 2);
+    }
 
-        Serial.print("Weight: ");
-        Serial.print(weight);
-        Serial.println(" g");
+    if (percentage >= 95)
+    {
+        pickupStatus = "Ready for pickup";
+        status = "Available";
+    }
+    else
+    {
+        pickupStatus = "Not ready yet";
+        status = "Full";
+    }
+    getLocation();
 
-        Serial.print("Weight: ");
-        Serial.print(kg);
-        Serial.println(" kg");
+    Serial.print("Weight: ");
+    Serial.print(weight);
+    Serial.println(" g");
 
-        Serial.print("Percentage: ");
-        Serial.print(percentage);
-        Serial.println(" %");
+    Serial.print("Kilos: ");
+    Serial.print(kg);
+    Serial.println(" kg");
 
-        Serial.print("Distance: ");
-        Serial.print(jarak);
-        Serial.println(" cm");
+    Serial.print("Percentage: ");
+    Serial.print(percentage);
+    Serial.println(" %");
 
-        Serial.print("Latitude: ");
-        Serial.println(latitude, 6);
-        Serial.print("Longitude: ");
-        Serial.println(longitude, 6);
+    Serial.print("Distance: ");
+    Serial.print(jarak);
+    Serial.println(" cm");
 
-        Serial.println();
+    Serial.print("Latitude: ");
+    Serial.println(latitude, 6);
+    Serial.print("Longitude: ");
+    Serial.println(longitude, 6);
 
-        xSemaphoreGive(printStatusSemaphore);
+    Serial.println();
+}
+
+void Seven_Segment()
+{
+    if (!isnan(weight) && !isnan(percentage))
+    {
+        disp.Initialize(10);
+        disp.Clear();
+        wdigit1 = int(kg) / 10;
+        wdigit2 = int(kg) % 10;
+        wfraction1 = int(kg * 10) % 10;
+        wfraction2 = int(kg * 100) % 10;
+
+        disp.Number(8, wdigit1);
+        disp.Numberdp(7, wdigit2);
+        disp.Number(6, wfraction1);
+        disp.Number(5, wfraction2);
+
+        pdigit1 = int(percentage) / 10;
+        pdigit2 = int(percentage) % 10;
+
+        disp.Number(3, pdigit1);
+        disp.Number(2, pdigit2);
+    }
+    else
+    {
+        disp.Clear();
     }
 }
 
-void taskGetSensorReading(void *parameter)
+void powerSwitching()
+{
+    bool hasSwitched = false;
+
+    while (!timeClient.update())
+    {
+        timeClient.forceUpdate();
+    }
+
+    int relayHour = timeClient.getHours();
+
+    if (relayHour >= 18 && relayHour < 7 && hasSwitched == false)
+    {
+        digitalWrite(RELAY, HIGH);
+        hasSwitched = true;
+    }
+    else if (relayHour >= 7 && relayHour < 18 && hasSwitched == true)
+    {
+        digitalWrite(RELAY, LOW);
+        hasSwitched = false;
+    }
+}
+
+void firebaseSetup()
+{
+    Serial.printf("Firebase Client v%s\n\n", FIREBASE_CLIENT_VERSION);
+
+    /* Assign the api key (required) */
+    config.api_key = API_KEY;
+
+    /* Assign the RTDB URL (required) */
+    config.database_url = DATABASE_URL;
+
+    /* Assign the sevice account credentials and private key (required) */
+    config.service_account.data.client_email = FIREBASE_CLIENT_EMAIL;
+    config.service_account.data.project_id = FIREBASE_PROJECT_ID;
+    config.service_account.data.private_key = PRIVATE_KEY;
+
+    /* Assign the callback function for the long running token generation task */
+    config.token_status_callback = tokenStatusCallback; // see addons/TokenHelper.h
+
+    // Comment or pass false value when WiFi reconnection will control by your code or third party library e.g. WiFiManager
+    Firebase.reconnectNetwork(true);
+
+    // Since v4.4.x, BearSSL engine was used, the SSL buffer need to be set.
+    // Large data transmission may require larger RX buffer, otherwise connection issue or data read time out can be occurred.
+    firebaseData.setBSSLBufferSize(12960 /* Rx buffer size in bytes from 512 - 16384 */, 12960 /* Tx buffer size in bytes from 512 - 16384 */);
+
+    // Or use legacy authenticate method
+    // config.database_url = DATABASE_URL;
+    // config.signer.tokens.legacy_token = "<database secret>";
+
+    // To connect without auth in Test Mode, see Authentications/TestMode/TestMode.ino
+
+    Firebase.begin(&config, &auth);
+}
+
+void dataLogging()
+{
+    bool hasUploaded = false; // indikator data log sudah dilakukan/belum
+    while (!timeClient.update())
+    {
+        timeClient.forceUpdate();
+    }
+
+    String time = timeClient.getFormattedTime();
+    currentHour = timeClient.getHours();
+    currentMinute = timeClient.getMinutes();
+    currentSecond = timeClient.getSeconds();
+    daynumber = timeClient.getDay();
+
+    dateFormat = timeClient.getFormattedDate();
+    date = dateFormat.substring(0, 10);
+
+    if (daynumber == 1)
+    {
+        day = "Mon";
+    }
+    else if (daynumber == 2)
+    {
+        day = "Tue";
+    }
+    else if (daynumber == 3)
+    {
+        day = "Wed";
+    }
+    else if (daynumber == 4)
+    {
+        day = "Thu";
+    }
+    else if (daynumber == 5)
+    {
+        day = "Fri";
+    }
+    else if (daynumber == 6)
+    {
+        day = "Sat";
+    }
+    else if (daynumber == 7)
+    {
+        day = "Sun";
+    }
+
+    Firebase.getInt(firebaseData, hourPath);
+    hour = firebaseData.intData();
+    Firebase.getInt(firebaseData, minutePath);
+    minute = firebaseData.intData();
+
+    Serial.print("Data Logging: ");
+    Serial.println(hasUploaded);
+    Serial.println("Scheduled time: ");
+    Serial.print(hour);
+    Serial.println(minute);
+    Serial.println("Current time: ");
+    Serial.print(currentHour);
+    Serial.println(currentMinute);
+
+    nextMinute = minute + 1 % 60;
+    if (nextMinute == 0)
+    {
+        nextHour = hour + 1 % 24;
+    }
+    else
+    {
+        nextHour = hour;
+    }
+
+    if (currentHour == hour && currentMinute == minute && currentSecond >= 0 && currentSecond <= 59 && hasUploaded == false)
+    {
+        Serial.print("we uploading data log\n");
+        Firebase.getInt(firebaseData, logIndexPath);
+        int logIndex = firebaseData.intData();
+        int nextIndex = logIndex + 1;
+        std::string indexPath = "/LogTest/" + std::to_string(logIndex);
+
+        json.clear();
+        json.set("/fullness", percentage);
+        json.set("/weight", kg);
+        json.set("/status", status);
+        json.set("/date", date);
+        json.set("/day", day);
+        json.set("/time", time);
+
+        Firebase.setJSON(firebaseData, indexPath, json);
+        Firebase.setInt(firebaseData, logIndexPath, nextIndex);
+        hasUploaded = true; // Set the flag to indicate that upload has been done
+    }
+    else if (currentHour == nextHour && currentMinute == nextMinute && currentSecond >= 0 && currentSecond <= 59 && hasUploaded == true)
+    {
+        Serial.print("ayy we done logging\n");
+        hasUploaded = false; // Reset the flag to allow upload in the next interval
+    }
+}
+
+void sendFirebase()
+{
+    Serial.print("Firebase connection status: ");
+    Serial.println(Firebase.ready() ? "true" : "false");
+    // Check if Firebase connection is ready
+    if (Firebase.ready())
+    {
+        if (percentage >= 95)
+        {
+            pickupStatus = "Ready for pickup";
+        }
+        else
+        {
+            pickupStatus = "Not ready yet";
+        }
+
+        json.clear();
+        json.set("/capacity1", percentage);
+        json.set("/weight1", kg);
+        json.set("/status", status);
+        json.set("/pick", pickupStatus);
+        json.set("/lat", latitude);
+        json.set("/long", longitude);
+        Firebase.updateNode(firebaseData, bin1Path, json);
+
+        if (pickupStatus != checkPickupStatus)
+        {
+            checkPickupStatus = pickupStatus;
+
+            if (checkPickupStatus == "Ready for pickup")
+            {
+                Firebase.getInt(firebaseData, bin1PickCount);
+                int counter = firebaseData.intData();
+                int counterPlus = counter + 1;
+                Firebase.setInt(firebaseData, bin1PickCount, counterPlus);
+            }
+            else if (checkPickupStatus == "Not ready yet")
+            {
+                Firebase.getInt(firebaseData, bin1PickCount);
+                int counter = firebaseData.intData();
+                int counterMin = counter - 1;
+                int updatedCounter = max(counterMin, 0);
+                Firebase.setInt(firebaseData, bin1PickCount, updatedCounter);
+            }
+        }
+        dataLogging();
+    }
+    else
+    {
+        Serial.println("Firebase not ready");
+        firebaseSetup();
+    }
+}
+
+void taskDataSerial(void *parameter)
 {
     while (1)
     {
-        ultrasonic();
-        GPS();
+        if (xSemaphoreTake(cSemaphore, portMAX_DELAY))
+        {
+            serialPrint();
+            xSemaphoreGive(cSemaphore);
+        }
+        else
+        {
+            Serial.println("Failed to take semaphore");
+        }
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
 
-void taskShowSegment(void *parameter)
+void taskSegment(void *parameter)
 {
     while (1)
     {
-        Seven_Segment();
-    }
-}
-
-void taskPrintStatus(void *parameter)
-{
-    while (1)
-    {
-        printStatus();
+        if (xSemaphoreTake(cSemaphore, portMAX_DELAY))
+        {
+            Seven_Segment();
+            xSemaphoreGive(cSemaphore);
+        }
+        else
+        {
+            Serial.println("Failed to take semaphore");
+        }
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
 
-void taskOpenBin(void *parameter)
+void taskPowerSwitch(void *parameter)
 {
     while (1)
     {
-        Open_Bin();
-        vTaskDelay(pdMS_TO_TICKS(100)); // Adjust the delay as needed
+        if (xSemaphoreTake(cSemaphore, portMAX_DELAY))
+        {
+            powerSwitching();
+            xSemaphoreGive(cSemaphore);
+        }
+        else
+        {
+            Serial.println("Failed to take semaphore");
+        }
+        vTaskDelay(pdMS_TO_TICKS(10000));
+    }
+}
+
+void taskFirebase(void *parameter)
+{
+    while (1)
+    {
+        if (xSemaphoreTake(mutex, portMAX_DELAY))
+        {
+            sendFirebase();
+            xSemaphoreGive(mutex);
+        }
+        else
+        {
+            Serial.println("Failed to take mutex");
+        }
+        vTaskDelay(pdMS_TO_TICKS(10000));
     }
 }
 
 void setup()
 {
     Serial.begin(115200);
+
+    // Relay
+    pinMode(RELAY, OUTPUT);
+    digitalWrite(RELAY, LOW);
 
     // UltraSonic
     pinMode(TRIG_PIN, OUTPUT);
@@ -173,29 +447,42 @@ void setup()
     pinMode(IR_PIN, INPUT);
 
     // Servo
-    pinMode(SERVO_PIN, OUTPUT);
     servo.attach(SERVO_PIN);
     servo.write(0); // Close the bin
 
     // HX711
+    Serial.println("Initializing Scale....");
     scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
-    scale.set_scale(18.51);
+    scale.set_scale(calib);
     scale.tare();
-    disp.Initialize(8);
+    Serial.println("Scale Initialized");
+    // 7 Segment
+    Serial.println("Initializing 7 Segment....");
+    disp.Initialize(10);
     disp.Clear();
-    Serial.println("Setup done");
+    Serial.println("7 Segment Initialized");
 
-    ultrasonicSemaphore = xSemaphoreCreateMutex();
-    openBinSemaphore = xSemaphoreCreateMutex();
-    printStatusSemaphore = xSemaphoreCreateMutex();
+    // GPS
+    ss.begin(GPS_BAUD);
 
-    xTaskCreate(taskOpenBin, "OpenBin", configMINIMAL_STACK_SIZE + 10240, NULL, 3, NULL);
-    xTaskCreate(taskGetSensorReading, "SensorReading", configMINIMAL_STACK_SIZE + 10240, NULL, 2, NULL);
-    xTaskCreate(taskShowSegment, "ShowSegment", configMINIMAL_STACK_SIZE + 10240, NULL, 4, NULL);
-    xTaskCreate(taskPrintStatus, "PrintStatus", configMINIMAL_STACK_SIZE + 10240, NULL, 1, NULL);
+    wifiSetup();
+
+    // NTP
+    timeClient.begin();
+    timeClient.update();
+    timeClient.setTimeOffset(25200);
+
+    firebaseSetup();
+    delay(20000); // Untuk nunggu token dari firebase
+
+    xTaskCreate(taskFirebase, "Firebase", configMINIMAL_STACK_SIZE + 20480, NULL, 0, NULL);
+    xTaskCreate(taskDataSerial, "Serial", configMINIMAL_STACK_SIZE + 4096, NULL, 0, NULL);
+    xTaskCreate(taskPowerSwitch, "Power", configMINIMAL_STACK_SIZE + 2048, NULL, 0, NULL);
+    xTaskCreate(taskSegment, "Segment", configMINIMAL_STACK_SIZE + 2048, NULL, 0, NULL);
+    // xTaskCreate(taskServo, "Servo", configMINIMAL_STACK_SIZE + 2048, NULL, 0, NULL);
 }
 
 void loop()
 {
-    // This loop can be empty since tasks are handling the functionality
+    Open_Bin();
 }
